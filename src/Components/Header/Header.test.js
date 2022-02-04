30a5f54b4cd410c38ca6eceddf6d289929bd8a6f
Frank Mckenna <fmckenna56330@gmail.com>
2022-02-04 16:35:34 +0000

    test: icon titles

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -7,7 +7,7 @@ describe('<Header />', () => {
     const header = screen.getByTestId('header');
     const headerLeft = screen.getByTestId('headerLeft');
     const headerRight = screen.getByTestId('headerRight');
-    const headerInput = screen.getByTestId('headerInput')
+    const headerInput = screen.getByTestId('headerInput');
 
     expect(header).toBeInTheDocument();
     expect(headerLeft).toBeInTheDocument();
@@ -15,18 +15,31 @@ describe('<Header />', () => {
     expect(headerInput).toBeInTheDocument();
   });
 
-  test('renders the header icons', () => {
+  test('renders the header icons and titles', () => {
     render(<Header />);
+    const header = screen.getByTestId('header');
     const SearchIcon = screen.getByTestId('SearchIcon');
     const HomeIcon = screen.getByTestId('HomeIcon');
     const SupervisorAccountIcon = screen.getByTestId('SupervisorAccountIcon');
     const BusinessCenterIcon = screen.getByTestId('BusinessCenterIcon');
     const NotificationsIcon = screen.getByTestId('NotificationsIcon');
 
+    const home = screen.getByText(/Home/i);
+    const myNetwork = screen.getByText(/My Network/i);
+    const jobs = screen.getByText(/Jobs/i);
+    const notifications = screen.getByText(/Notification/i);
+    const me = screen.getAllByText(/me/i);
+
     expect(SearchIcon).toBeInTheDocument();
     expect(HomeIcon).toBeInTheDocument();
     expect(SupervisorAccountIcon).toBeInTheDocument();
     expect(BusinessCenterIcon).toBeInTheDocument();
     expect(NotificationsIcon).toBeInTheDocument();
+
+    expect(home).toBeInTheDocument();
+    expect(myNetwork).toBeInTheDocument();
+    expect(jobs).toBeInTheDocument();
+    expect(notifications).toBeInTheDocument();
+    expect(header).toContainHTML('me');
   })
 })
