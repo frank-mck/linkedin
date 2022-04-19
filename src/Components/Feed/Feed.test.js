6b7c26eb78a5b14203a6886570d8b2752f7ec16c
Frank Mckenna <fmckenna56330@gmail.com>
2022-04-19 11:59:15 +0100

    test: include feedInputOption tests in Feed.test.js

---
+++ src/Components/Feed/Feed.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react"
+import Feed from "./Feed";
+
+describe("<Feed />", () => {
+  test("Renders an input option", () => {
+    render(<Feed />);
+    const inputOption = screen.queryAllByTestId("inputOption")[0];
+    const inputOptionTitle = screen.queryAllByTestId("inputOption-title")[0];
+    const inputOptionIcon = screen.queryAllByTestId("inputOptionIcon")[0];
+
+    expect(inputOption).toBeInTheDocument();
+    expect(inputOptionTitle).toBeInTheDocument();
+    expect(inputOptionIcon).toBeInTheDocument();
+  })
+})
